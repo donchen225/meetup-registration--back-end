3a7510b31e5924a6ddc023b898d30017bf322e07
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-02-15 17:31:05 -0800

    completed

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const attendeeSchema = new mongoose.Schema({
-  // your code here
+  id: { type: Number, required: true, unique: true }
+  firstName: { type: String, required: true }
+  lastName: { type: String, required: true }
+  email: { type: String, required: true, unique: true }
+  shirt: { type: String, required: true, enum: ["XS", "S", "M", "L", "XL", "XXL"]},
+  skillLevel: { type: String, required: true, enum: ["beginner", "intermediate", "expert"]}
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
